047fb05a2cb6372ee72fcc0230a249be0a5f1a27
Mohammad Shahjalal Khan <shahjalalkhanrabbi@gmail.com>
2023-08-20 21:27:41 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  const [decrement, setDecrement] = useState(50);
+
+  const decrementClick = () => {
+    setCount(decrement - 1);
+  };
+  return (
+    <>
+      <h1>Counter App</h1>
+      <button onClick={handleClick}>Add</button>
+      <div>{count}</div>
+      <div>
+      <button onClick={decrementClick}>Decrement</button>
+    <div>{setDecrement}</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
